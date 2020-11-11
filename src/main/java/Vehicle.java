30f3796a61562a9a10ab9da935acd825f65c656e
Kasia Trzcionka <trzcionkakatarzyna@gmail.com>
2020-11-11 19:51:00 +0100

    Java task 1

---
+++ src/main/java/Vehicle.java
@@ -0,0 +1,11 @@
+public class Vehicle {
+    public String type;
+    public Integer max_speed;
+    public String producer;
+
+    public Vehicle(String ty, Integer ms, String pr){
+        type = ty;
+        max_speed = ms;
+        producer = pr;
+    }
+}
